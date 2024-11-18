426c1535a03f0401fc61a28dfdb8fc623c87bc77
Minden Áron <you@inf.unideb.hu>
2024-11-18 13:39:38 +0100

    Snapshot1

--- src/main/java/hu/unideb/inf/bootstrap/controller/MainController.java
+++ src/main/java/hu/unideb/inf/bootstrap/controller/MainController.java
@@ -1,8 +1,31 @@
 package hu.unideb.inf.bootstrap.controller;
 
+import hu.unideb.inf.bootstrap.model.Person;
+import hu.unideb.inf.bootstrap.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @GetMapping("")
+    public String showHomepage(){
+        return "index";
+    }
+
+    @GetMapping("/persons")
+    public String showAllPersons(Model model){
+        List<Person> personsList = personRepository.findAll();
+        model.addAttribute("personsList", personsList);
+        return "persons";
+    }
+
 
 }
