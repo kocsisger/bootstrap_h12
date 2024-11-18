9a0314f61792ec4f3a8f144262572bc4e4a2001f
Minden Áron <you@inf.unideb.hu>
2024-11-18 12:18:14 +0100

    Initial commit

---
+++ src/test/java/hu/unideb/inf/bootstrap/BootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
