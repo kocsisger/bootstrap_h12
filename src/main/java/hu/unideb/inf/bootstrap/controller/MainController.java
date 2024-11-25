66c7e4e2053bb4e5fedf4c603826658485167b9c
Minden Áron <you@inf.unideb.hu>
2024-11-25 13:37:11 +0100

    Final

--- src/main/java/hu/unideb/inf/bootstrap/controller/MainController.java
+++ src/main/java/hu/unideb/inf/bootstrap/controller/MainController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -27,5 +30,32 @@ public class MainController {
         return "persons";
     }
 
+    @GetMapping("/persons/new")
+    public String newPerson(Model model){
+        model.addAttribute("newPerson", new Person());
+        model.addAttribute("pageTitle", "Add new person");
+        return "newPersonForm";
+    }
+
+    @GetMapping("/persons/edit/{id}")
+    public String editPerson(@PathVariable Long id, Model model){
+        model.addAttribute("newPerson", personRepository.findById(id));
+        model.addAttribute("pageTitle", "Edit person");
+        return "newPersonForm";
+    }
+
+    @PostMapping("/persons/save")
+    public String savePerson(Person newPerson, RedirectAttributes rd){
+        personRepository.save(newPerson);
+        rd.addFlashAttribute("message", "Saved");
+        return "redirect:/persons";
+    }
+
+    @GetMapping("/persons/delete/{id}")
+    public String deletePerson(@PathVariable Long id, RedirectAttributes rd){
+        personRepository.deleteById(id);
+        rd.addFlashAttribute("message", "Deleted");
+        return "redirect:/persons";
+    }
 
 }
